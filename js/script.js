0081e324175abf25a9750852ac02423dc4372da6
evsem <frukwork@gmail.com>
2021-11-27 21:18:31 +0300

    v(2)-project_1

--- js/script.js
+++ js/script.js
@@ -1,1 +1,32 @@
-console.log('Hello Console.')
+//console.log('Hello Console.')
+
+//Arrow function
+let arrFun = (a, b) => {
+  return (a + b) * 5
+}
+
+//Function
+function classicFin(text_user) {
+  console.log(`Hello ${text_user}`)
+}
+
+//Object
+const user_1 = {
+  name: 'Alex',
+  age: 23,
+  job: false,
+  adress: {
+    town: 'London',
+    numberHouse: 13,
+  },
+  hasFamily: true,
+  compositionFamily: [
+    (sister = { name: 'Sally', age: 26 }),
+    (mother = { name: 'Go', age: 54, job: true }),
+    (brother = { name: 'Sally', age: 15 }),
+  ],
+}
+
+//Array
+
+//Classes
